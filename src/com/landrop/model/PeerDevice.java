f83d595bef8405e0185843a5dff243d30157a8a5
ask-io <abhijit.uni29@gmail.com>
2026-09-13 15:31:05 +0530

    feat(model): add tcp port field and getters to PeerDevice

--- src/com/landrop/model/PeerDevice.java
+++ src/com/landrop/model/PeerDevice.java
@@ -3,39 +3,58 @@ package com.landrop.model;
 import java.net.InetAddress;
 
 public class PeerDevice {
-	private final InetAddress ipAddress;
+    private final InetAddress ipAddress;
     private final String hostname;
+    private int port;
     private long lastSeenTimestamp;
     private boolean isTrusted;
     
     public PeerDevice(InetAddress ipAddress, String hostname) {
+        this(ipAddress, hostname, 0);
+    }
+
+    public PeerDevice(InetAddress ipAddress, String hostname, int port) {
         this.ipAddress = ipAddress;
         this.hostname = hostname;
+        this.port = port;
         this.lastSeenTimestamp = System.currentTimeMillis();
         this.isTrusted = false;
     }
     
     public InetAddress getIpAddress() { 
-    	return ipAddress;
+        return ipAddress;
     }
     
     public String getHostname() {
-    	return hostname;
+        return hostname;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
     
     public long getLastSeenTimestamp() {
-    	return lastSeenTimestamp;
+        return lastSeenTimestamp;
     }
     
     public void updateLastSeen() {
-    	this.lastSeenTimestamp = System.currentTimeMillis();
+        this.lastSeenTimestamp = System.currentTimeMillis();
     }
     
     public boolean isTrusted() {
-    	return isTrusted;
+        return isTrusted;
     }
     
     public void setTrusted(boolean trusted) {
-    	isTrusted = trusted;
+        isTrusted = trusted;
+    }
+
+    @Override
+    public String toString() {
+        return hostname + " (" + ipAddress.getHostAddress() + ":" + port + ")";
     }
 }
